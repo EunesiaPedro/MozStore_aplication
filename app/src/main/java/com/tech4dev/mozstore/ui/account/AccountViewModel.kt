7589899401ea1963ef39ec2a52ba3559dd752020
USER <yunesya@gmail.com>
2022-09-26 18:01:18 +0200

    My store App first commit

---
+++ app/src/main/java/com/tech4dev/mozstore/ui/account/AccountViewModel.kt
@@ -0,0 +1,7 @@
+package com.tech4dev.mozstore.ui.account
+
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/tech4dev/mozstore/ui/favourites/FavouritesViewModel.kt
@@ -0,0 +1,7 @@
+package com.tech4dev.mozstore.ui.favourites
+
+import androidx.lifecycle.ViewModel
+
+class FavouritesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
